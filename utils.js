3dd9443767d20e65f73b6b00e04aeb1650706d3b
Samir Hosny Mohamed <samir7osny@yahoo.com>
2020-05-22 06:41:35 +0200

    test

--- utils.js
+++ utils.js
@@ -24,6 +24,7 @@ let get_seconds_duration = (seconds) => {
 let search = async(query) => {
     logger.log(`search about "${query}"`)
     const data = await ytsr(query, { limit: 1 })
+	console.log(data)
     data.items = data.items.map((item) => {
         item['secs'] = get_duration_seconds(item['duration'])
         return item
